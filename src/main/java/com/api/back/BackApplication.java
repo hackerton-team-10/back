4d1a33c653d6121dbefbdf667017dd9030807134
antidote <ljh8651@nate.com>
2025-02-11 11:49:49 +0900

    init

---
+++ src/main/java/com/api/back/BackApplication.java
@@ -0,0 +1,13 @@
+package com.api.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackApplication.class, args);
+    }
+
+}
