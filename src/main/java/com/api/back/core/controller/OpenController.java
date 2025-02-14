493c057e6a93162565cc35e3e52f3fbc96de0f2c
Blockmonkey <blockmonkey@naver.com>
2025-02-14 12:42:36 +0900

    feat: health check api

---
+++ src/main/java/com/api/back/core/controller/OpenController.java
@@ -0,0 +1,26 @@
+package com.api.back.core.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/open")
+@Tag(name = "Open API (no auth)")
+public class OpenController {
+    @GetMapping("/health")
+    @Operation(summary = "Health Check (0)", description = "Health Check API")
+    @ApiResponse(responseCode = "200", description = "Success", content = {@Content(mediaType = "application/json", schema = @Schema(type = "boolean"))})
+    public boolean healthCheck() {
+        return true;
+    }
+
+}
