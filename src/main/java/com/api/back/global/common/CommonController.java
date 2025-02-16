db07b34ab4947fd666140473350ae76f7f03615a
Blockmonkey <blockmonkey@naver.com>
2025-02-16 19:54:28 +0900

    fix: change health check api endpoint

--- src/main/java/com/api/back/global/common/CommonController.java
+++ src/main/java/com/api/back/global/common/CommonController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/open")
+@RequestMapping("/v1")
 @Tag(name = "Open API (no auth)")
 public class CommonController {
 
