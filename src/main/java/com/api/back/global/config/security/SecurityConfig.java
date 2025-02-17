013cbf54facbb1fdb97e2e4e025f96279bbb6229
Be-HinD <kr100475@gmail.com>
2025-02-17 22:32:19 +0900

    [fix] /v1 경로 인가작업

--- src/main/java/com/api/back/global/config/security/SecurityConfig.java
+++ src/main/java/com/api/back/global/config/security/SecurityConfig.java
@@ -43,7 +43,8 @@ public class SecurityConfig {
                     "/error",
                     "/swagger-ui/**",
                     "/swagger-resources/**",
-                    "/api-docs/**"
+                    "/api-docs/**",
+                    "/v1/**"
                 );
         };
     }
